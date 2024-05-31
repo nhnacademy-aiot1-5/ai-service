9b1337289332ec38bc5d0fbda62dc3ad41f0b367
dmswl <siddltkfkd@gmail.com>
2024-05-31 14:40:29 +0900

    refactor: 공용 db 대신 개인 db를 사용하도록 변경
    공용 db를 자꾸 초기화시켜 값이 자꾸 비어버리는 이슈 해결

--- main/python/live/ioteatime/ai_service/outlier/redis_r.py
+++ main/python/live/ioteatime/ai_service/outlier/redis_r.py
@@ -8,10 +8,9 @@ properties.read('./config.ini')
 
 host = properties['REDIS']['host']
 port = properties['REDIS']['port']
-password = properties['REDIS']['password']
 db = properties['REDIS']['db']
 
-r = redis.Redis(host=host, port=port, password=password, db=db)
+r = redis.Redis(host=host, port=port, db=db)
 
 def set(name, dict):
     r.set(name, json.dumps(dict))
